124a48ea421edd0f457fc3252b6683accacbd544
Nicolai Hollup Brand <nicolai.brand@ntnu.no>
2023-04-21 12:32:40 +0200

    feat: 404 page

--- src/components/Feedback/unknownErrorHandler.ts
+++ src/components/Feedback/unknownErrorHandler.ts
@@ -1,30 +1,42 @@
 import { ApiError as BackendApiError } from '@/api';
 import { AxiosError } from 'axios';
 
+export type ResponseError = {
+  message: string;
+  statusCode: number;
+};
+
 /**
  * Finds the correct error message for the given error
  * The message is then put into the error store
  * which an error component can then display
  * @param error The unknown error to handle
  */
-const handleUnknownError = (error: any): string => {
+export const handleUnknownError = (error: any): ResponseError => {
+  let msg = 'Det oppsto en feil med behandling av forespørselen din.';
+  let statusCode: number = 500;
   if (error instanceof AxiosError) {
+    statusCode = error.response?.status ?? 500;
     switch (error.code) {
       case AxiosError.ERR_NETWORK:
-        return 'Det oppsto en nettverksfeil. Vennligst sjekk nettverksforbindelsen din og prøv igjen.';
+        msg =
+          'Det oppsto en nettverksfeil. Vennligst sjekk nettverksforbindelsen din og prøv igjen.';
       case AxiosError.ERR_BAD_REQUEST:
-        return 'Det oppsto en feil med behandling av forespørselen din.';
+        msg = 'Det oppsto en feil med behandling av forespørselen din.';
       case AxiosError.ERR_CANCELED:
-        return 'Forespørselen ble avbrutt.';
+        msg = 'Forespørselen ble avbrutt.';
       case AxiosError.ETIMEDOUT:
-        return 'Forespørselen tok for lang tid. Vennligst prøv igjen.';
+        msg = 'Forespørselen tok for lang tid. Vennligst prøv igjen.';
       default:
-        return 'Vi klarte ikke å videreføre din forespørsel riktig';
+        msg = 'Vi klarte ikke å videreføre din forespørsel riktig';
     }
   } else if (error instanceof BackendApiError) {
-    return error.body?.detail ?? error.body;
+    msg = error.body?.detail ?? error.body;
+    statusCode = error.status;
   }
-  return 'Det oppsto en feil med behandling av forespørselen din.';
+
+  return { message: msg, statusCode: statusCode };
 };
 
-export default handleUnknownError;
+//export default handleUnknownError;
+//export default ResponseError;
---
+++ src/views/NotFound404View.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1 data-testid="not-found-header">Kunne ikke finne siden :-(</h1>
+  <div v-if="errorMsg">
+    <p>årsak: {{ errorMsg }}</p>
+  </div>
+  <div>
+    <router-link :to="{ name: 'base' }" data-testid="goto-main">til hovedside</router-link>
+  </div>
+  <div>
+    <!-- the 'to' prop is used for fallback -->
+    <router-link @click="$router.go(-1)" :to="{ name: 'base' }" data-testid="goback"
+      >tilbake</router-link
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter, useRoute } from 'vue-router';
+
+const route = useRoute();
+const errorMsg = route.query.errorMsg as string;
+//const props = defineProps({
+//  errorMsg: {
+//    type: String,
+//    required: false,
+//  },
+//});
+</script>
